50277342220ca1a9bea7dfdf5e7500de3fac47d4
lemphis <lemphis@gmail.com>
2025-02-17 23:51:44 +0900

    Add SQL initialization files
    * table 생성 DDL script file(schema.sql) 추가
    * seed data 적재 DML script file(data.sql) 추가

---
+++ migrations/schema.sql
@@ -0,0 +1,55 @@
+CREATE TABLE IF NOT EXISTS vets (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    INDEX(last_name)
+) engine = InnoDB;
+
+CREATE TABLE IF NOT EXISTS specialties (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(80),
+    INDEX(name)
+) engine = InnoDB;
+
+CREATE TABLE IF NOT EXISTS vet_specialties (
+    vet_id INT UNSIGNED NOT NULL,
+    specialty_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (vet_id) REFERENCES vets(id),
+    FOREIGN KEY (specialty_id) REFERENCES specialties(id),
+    PRIMARY KEY (vet_id, specialty_id)
+) engine = InnoDB;
+
+CREATE TABLE IF NOT EXISTS types (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(80),
+    INDEX(name)
+) engine = InnoDB;
+
+CREATE TABLE IF NOT EXISTS owners (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    address VARCHAR(255),
+    city VARCHAR(80),
+    telephone VARCHAR(20),
+    INDEX(last_name)
+) engine = InnoDB;
+
+CREATE TABLE IF NOT EXISTS pets (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30),
+    birth_date DATE,
+    type_id INT UNSIGNED NOT NULL,
+    owner_id INT UNSIGNED,
+    INDEX(name),
+    FOREIGN KEY (owner_id) REFERENCES owners(id),
+    FOREIGN KEY (type_id) REFERENCES types(id)
+) engine = InnoDB;
+
+CREATE TABLE IF NOT EXISTS visits (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    pet_id INT UNSIGNED,
+    visit_date DATE,
+    description VARCHAR(255),
+    FOREIGN KEY (pet_id) REFERENCES pets(id)
+) engine = InnoDB;
