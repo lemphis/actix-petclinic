38b62f714efac645c316ba8e65ded7366ea2c54a
lemphis <lemphis@gmail.com>
2025-02-18 23:00:34 +0900

    Add error.html page and connect GET /oups handler
    * error page로 사용할 error.html 추가
    * GET /oups handler를 ServiceConfig에 연결

--- src/web/mod.rs
+++ src/web/mod.rs
@@ -1,8 +1,10 @@
 use actix_web::web::ServiceConfig;
-use vet_handler::{show_resources_vet_list, show_vet_list};
 
+pub mod error_handler;
 pub mod vet_handler;
 
 pub fn configure_route(cfg: &mut ServiceConfig) {
-    cfg.service(show_resources_vet_list).service(show_vet_list);
+    cfg.service(vet_handler::show_resources_vet_list)
+        .service(vet_handler::show_vet_list)
+        .service(error_handler::trigger_error);
 }
