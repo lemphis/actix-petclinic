14607b896ecbcf9155a2f5110e1d5750226c4578
lemphis <lemphis@gmail.com>
2025-03-05 00:01:14 +0900

    Register `FlashMessagesFramework` middleware on `App`
    * flash message를 주고받기 위해 FlashMessagesFramework를 App의 middleware로 등록

--- src/config/server.rs
+++ src/config/server.rs
@@ -1,14 +1,20 @@
 use crate::{web, AppState};
 use actix_files::Files;
-use actix_web::{middleware, web::Data, App, HttpServer};
+use actix_web::{cookie::Key, middleware, web::Data, App, HttpServer};
+use actix_web_flash_messages::{storage::CookieMessageStore, FlashMessagesFramework};
 
 pub async fn start_server(app_state: AppState) -> std::io::Result<()> {
+    let signing_key = Key::generate();
+    let message_store = CookieMessageStore::builder(signing_key).build();
+    let message_framework = FlashMessagesFramework::builder(message_store).build();
+
     HttpServer::new(move || {
         App::new()
             .app_data(Data::new(app_state.clone()))
             .wrap(middleware::Logger::default())
             .wrap(middleware::NormalizePath::trim())
             .wrap(middleware::Compress::default())
+            .wrap(message_framework.clone())
             .service(Files::new("/static", "./static").show_files_listing())
             .configure(web::configure_route)
     })
