7af569b9f7fd6a36574e2a0bb76d4370247f7158
lemphis <lemphis@gmail.com>
2025-03-07 18:37:31 +0900

    Add common error handler function for JSON error responses
    * 에러 공통 응답 처리를 위해 `error_handler` 함수 추가

--- src/config/server.rs
+++ src/config/server.rs
@@ -1,6 +1,12 @@
-use crate::{web, AppState};
+use crate::{model::error_response::ErrorResponse, web, AppState};
 use actix_files::Files;
-use actix_web::{cookie::Key, middleware, web::Data, App, HttpServer};
+use actix_web::{
+    cookie::Key,
+    dev::ServiceResponse,
+    middleware::{self, ErrorHandlerResponse},
+    web::Data,
+    App, Error, HttpResponse, HttpServer, Result,
+};
 use actix_web_flash_messages::{storage::CookieMessageStore, FlashMessagesFramework};
 
 pub async fn start_server(app_state: AppState) -> std::io::Result<()> {
@@ -21,4 +27,24 @@ pub async fn start_server(app_state: AppState) -> std::io::Result<()> {
     .bind("127.0.0.1:8080")?
     .run()
     .await
+}
+
+fn error_handler<B>(res: ServiceResponse<B>) -> Result<ErrorHandlerResponse<B>> {
+    let error_message = res
+        .response()
+        .error()
+        .map(Error::to_string)
+        .unwrap_or_else(|| format!("HTTP {}", res.status()));
+    let path = res.request().uri().path().to_owned();
+
+    let error_response = ErrorResponse::new(error_message, path);
+
+    let new_response = HttpResponse::build(res.status())
+        .content_type("application/json")
+        .json(error_response)
+        .map_into_right_body();
+
+    let new_res = ErrorHandlerResponse::Response(res.into_response(new_response));
+
+    Ok(new_res)
 }
