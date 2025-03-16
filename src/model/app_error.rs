97eee23ba74c85f0db6a82fc1d5e9a6d9d6381c1
lemphis <lemphis@gmail.com>
2025-03-16 17:27:43 +0900

    Add `ResourceIdMismatchError` to AppError
    * 요청 path의 resouece id와 body의 resource id가 다른 케이스 처리를 위해 AppError에 ResourceIdMismatchError 추가

--- src/model/app_error.rs
+++ src/model/app_error.rs
@@ -8,6 +8,15 @@ pub enum AppError {
     #[error("Resource not found: {resource} with id: {id}")]
     ResourceNotFound { resource: String, id: u32 },
 
+    #[error(
+        "Resource ID mismatch: {resource} - Path ID {path_id} does not match Body ID {body_id}"
+    )]
+    ResourceIdMismatch {
+        resource: String,
+        path_id: u32,
+        body_id: u32,
+    },
+
     #[error("Database error: {0}")]
     DbError(#[from] sea_orm::DbErr),
 
@@ -21,7 +30,8 @@ pub enum AppError {
 impl ResponseError for AppError {
     fn error_response(&self) -> HttpResponse {
         let mut res_builder = match self {
-            AppError::ResourceNotFound { resource: _, id: _ } => HttpResponse::NotFound(),
+            AppError::ResourceNotFound { .. } => HttpResponse::NotFound(),
+            AppError::ResourceIdMismatch { .. } => HttpResponse::BadRequest(),
             AppError::DbError(_) => HttpResponse::InternalServerError(),
             AppError::TemplateError(_) => HttpResponse::InternalServerError(),
             AppError::SerializeError(_) => HttpResponse::InternalServerError(),
